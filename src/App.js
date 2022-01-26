51e1c1809057ce7fb0f5dfda004b932b71731229
akhilesh rawat <akhileshrawat76@gmail.com>
2022-01-26 11:48:06 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import "./App.css";
+
+const api = {
+  key: "2e2dce9f3baa8bad918e42683fae3480",
+  base: "http://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [error, setError] = useState("");
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          if (result.cod === 200) {
+            setWeather(result);
+            setError("");
+          } else {
+            console.log(result.message);
+            setError(result.message);
+            setWeather({})
+          }
+          setQuery("");
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    const day = days[d.getDay()];
+    const month = months[d.getMonth()];
+    const date = d.getDate();
+    const year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box warm">
+          <input
+            type="text"
+            className="search-bar"
+            name=""
+            id=""
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main !== "undefined" ? (
+          <>
+          {console.log(weather.main)}
+            <div className="location-box">
+              <div className="location">
+                {weather.name},{weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].description}</div>
+            </div>
+          </>
+        ) : (
+          <div className="weather-box">
+            <div className="weather">{error.toUpperCase()}</div>
+          </div>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
